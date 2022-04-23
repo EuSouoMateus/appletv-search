54fb8e3b3e707c6ff8d71531e2f6e45f8920a4ff
MateLopes <mateus08araujo@gmail.com>
2022-04-23 15:01:54 -0300

    factoring code

--- js/script.js
+++ js/script.js
@@ -1,28 +1,44 @@
+const api_itunes = "https://uts-api.itunes.apple.com/uts/v2/search/incremental?";
+const form = document.getElementById("AppleTVSearch");
+const locales = {
+    '143503': 'pt-BR',
+    '143441': 'en-US',
+    '143444': 'en-GB'
+}
+
+String.prototype.format = function(args) {
+    let text = this;
+    for(var attr in args){
+        text = text.split('{' + attr + '}').join(args[attr]);
+    }
+    return text;
+};
+
+function copyClipboard(target) {
+    navigator.clipboard.writeText(target.innerHTML);
+}
+
 window.addEventListener("load", function() {
     async function fetchAsync(url, query_search, storefront, language_locale) {
         let response = await fetch(url + new URLSearchParams({
-            sf: storefront, // storefront code
-            locale: language_locale, // language locale
+            sf: storefront,
+            locale: language_locale,
             caller: 'wta',
             utsk: '6e3013c6d6fae3c2::::::235656c069bb0efb',
-            v: '56', // version
-            pfm: 'web', // platform
-            q: query_search // movie name
+            v: '56',
+            pfm: 'web',
+            q: query_search
         }));
         return await response.json();
     }
 
-    String.prototype.format = function(args) {
-        let text = this;
-        for(var attr in args){
-            text = text.split('{' + attr + '}').join(args[attr]);
-        }
-        return text
-    };
-
-    const api_itunes = "https://uts-api.itunes.apple.com/uts/v2/search/incremental?";
-    const locale = navigator.languages[0];
-    const form = document.getElementById("AppleTVSearch");
+    function makeHTML(){
+        let html = '{div}{href}{h2}';
+        let div = '<div class="movie-result"><div class="title-year"><h1>{title}{year}</h1></div>';
+        let href = '<a href="{href}" target="_blank" class="imglink"><img src="{src}" alt="Artwork for {title}{year}"></a>';
+        let h2 = '<h2 class="apple-id" title="Copy Apple Id to Clipboard" onclick="copyClipboard(this)">{id}</h2>';
+        return html.format({'div': div, 'href': href, 'h2': h2});
+    }
 
     function renderResult(data={}){
         let div_results = document.getElementById("results");
@@ -30,17 +46,17 @@ window.addEventListener("load", function() {
             div_results.innerHTML = "<h2>No results found.</h2>";
             return;
         }
-        // results success
+
         div_results.innerHTML = "";
-        let html_img = '<div class="movie-result"><div class="title-year"><h1>{title}{year}</h1></div><a href="{href}" target="_blank" class="imglink"><img src="{src}" alt="Artwork for {title}{year}"></a><h2>{id}</h2>';
+        let html = makeHTML();
         let shelves = data.canvas.shelves[0].title.toLowerCase() === 'filmes' ? data.canvas.shelves[0] : data.canvas.shelves[1];
         shelves.items.forEach(result => {
             let releaseYear = new Date(result.releaseDate).getFullYear();
             releaseYear = releaseYear ? ' ' + releaseYear : '';
             let image_url = result.images.coverArt16X9.url.format(
-                {'w': result.images.coverArt16X9.width, 'h': result.images.coverArt16X9.height, 'f': 'jpg'}
+                {'w': '1920', 'h': '1080', 'f': 'jpg'}
             );
-            div_results.innerHTML += html_img.format({'href': result.url, 'src': image_url, 'id': result.id, 'title': result.title, 'year': releaseYear});
+            div_results.innerHTML += html.format({'href': result.url, 'src': image_url, 'id': result.id, 'title': result.title, 'year': releaseYear});
         });
     }
 
@@ -51,6 +67,7 @@ window.addEventListener("load", function() {
         if(!query_search){
             return;
         }
+        let locale = locales[store_front] || navigator.language || navigator.userLanguage;
         fetchAsync(api_itunes, query_search, store_front, locale)
         .then(response => renderResult(response.data)
         )
